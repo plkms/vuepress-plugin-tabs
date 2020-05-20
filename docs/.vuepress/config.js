cf1068f3ec36e99acd3a5dd01bd27842af80b963
plkms <ext+ghub@mailbox.org>
2020-05-20 20:13:49 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   title: 'element-tabs',
   description: "markdown custom container to display content in tabs",
   themeConfig: {
-    repo: "https://github.com/superbiger/vuepress-plugin-tabs",
+    repo: "https://github.com/plkms/vuepress-plugin-tabs",
     editLinks: true,
     docsDir: 'docs',
     editLinkText: 'Edit this page on GitHub',
